0a361528627cbb2443f35af8fbb713f54307dbef
psaavala <c3sahe00@students.oamk.fi>
2024-04-10 10:02:13 +0300

    added some text for quizzes and questionnary propositions

---
+++ js/vikings.js
@@ -0,0 +1,24 @@
+/*Viikingit muistetaan keskuudessamme pohjoismaisina hurjina sotureina, ja useimmat
+aiheesta kertovat pelit, elokuvat ja tarinat keskittyvätkin suurimmaksi osaksi tähän
+heidän hurjaan puoleensa. Viikingit olivat kuitenkin ennen kaikkea menestyneitä kauppiaita 
+ja liikkeenharjoittajia. He saivat elantonsa pitämällä maatiloja,
+kalastamalla, metsästämällä, kauppaamalla tekemiänsä 
+tuotteita sekä valloittamalla uusia alueita. 
+
+"kuuluisin viikinkikaupunki"
+
+Viikingit olivat pohjimmiltaan kuitenkin maalaisyhteisöjä, joissa perhe ja oman maapalstan
+omistaminen näyttelivät tärkeintä roolia. Maan omistaminen oli kaikkein tärkeintä, koska 
+se määritteli ihmisen aseman yhteisössä. Iso perhe ja maatila olikin kaikista tavallisin 
+näky viikinkiyhteisöissä. Naiset olivat kodin ylipäälliköitä, ja miehet hoitivat 
+kaupankäynnin, pellonhoidon ja yhteiskunnalliset toiminnot.
+
+"kuuluisin Jaarli"
+
+Viikinkien yhteiskunnassa oli kolme erilaista luokkaa. Ihmiset jaoteltiin Jaarleihin, 
+aatelisiin ja vapaisiin ihmisiin. Suurin erottava tekijä yhteiskunnassa oli, oliko
+ihminen vapaa ihminen vai ei. Vapaalla ihmisellä oli oikeus kantaa asetta ja 
+osallistua sekä vaikuttaa yhteiskunnan toimintoihin. Ei-vapaat ihmiset olivat yleensä 
+maatiloilla työskenteleviä orjia.
+
+"rannekorukysymys-vapaa ihminen?"*/
