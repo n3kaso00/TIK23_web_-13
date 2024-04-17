467fc6f99209417cf3d2831abe53d1fbdb5bbb5f
psaavala <c3sahe00@students.oamk.fi>
2024-04-17 14:41:29 +0300

    added comments, moved questions to trivia part, made feedback to answers to display in feedbackbox as intended originally, made feedback box empty before answering questions

--- js/vikings.js
+++ js/vikings.js
@@ -7,6 +7,8 @@ tuotteita sekä valloittamalla uusia alueita.
 
 "Mille aikavälille Viikinkien aikakausi sijoittui?"  "650-859 ,, 793-1066 ,, 823-1145"
 
+----------------------------------------------------------------------------------------------
+
 Viikingit olivat pohjimmiltaan kuitenkin maalaisyhteisöjä, joissa perhe ja oman maapalstan
 omistaminen näyttelivät tärkeintä roolia. Maan omistaminen oli kaikkein tärkeintä, koska 
 se määritteli ihmisen aseman yhteisössä. Iso perhe ja maatila olikin kaikista tavallisin 
@@ -16,24 +18,29 @@ kaupankäynnin, pellonhoidon ja yhteiskunnalliset toiminnot.
 "kuuluisin viikinkikaupunki HEDEBY/Oslo/Uppsala, Hedeby suurin kauppakaupunki,
  Oslo ns. viikinkien pääkaupunki, Uppsala uskonnollinen keskus"
  
-"mitkä esineet antoivat korkean paikan yhteiskunnassa? avain/miekka"
 
+-----------------------------------------------------------------------------------------------
 
 Viikinkien pitämiin eläimiin kuuluivat esimerkiksi vuohet, lampaat, siat, 
 lampaat ja ankat. Niitä vaalittiin suuresti, sillä ne tuottivat Viikingeille ruokaa ja erilaisia, 
-hyötytarvikkeita. Heillä oli myös lemmikkieläimiä (koiria ja kissoja) kuten meilläkin.
+hyötytarvikkeita. Heillä oli myös lemmikkieläimiä (koiria ja kissoja) kuten meilläkin. 
+He elivät luonnosta, kuten muutkin tuon ajan ihmiset ja muodostivat vahvan suhteen luontoon.
+
 Viikinkien jumaltarustoon kuitenkin kuului eräs tärkeä susieläin. Mikä on tämän suden nimi?
 
 "FENRIR/Jörmundgandr/Gullinbursti (selitykset puuttuu)"
 
-Viikinkien yhteiskunnassa oli kolme erilaista luokkaa. Ihmiset jaoteltiin Jaarleihin, 
-aatelisiin ja vapaisiin ihmisiin. Suurin erottava tekijä yhteiskunnassa oli, oliko
-ihminen vapaa ihminen vai ei. Vapaalla ihmisellä oli oikeus kantaa asetta ja 
-osallistua sekä vaikuttaa yhteiskunnan toimintoihin. Ei-vapaat ihmiset olivat yleensä 
-maatiloilla työskenteleviä orjia.
+------------------------------------------------------------------------------------------------
 
-"rannekorukysymys-vapaa ihminen?"
+Viikinkien yhteiskunnassa oli kolme erilaista luokkaa kansalaisille. Ihmiset jaoteltiin Jaarleihin, 
+aatelisiin ja vapaisiin ihmisiin. Suurin erottava tekijä yhteiskunnassa oli, oliko
+henkilö vapaa ihminen vai ei. Vapaalla ihmisellä oli oikeus kantaa asetta ja 
+osallistua sekä vaikuttaa yhteiskunnan toimintoihin. Ei-vapaat ihmiset olivat yleensä 
+maatiloilla työskenteleviä orjia ja työläisiä.
+
 "kuuluisin Jaarli"
+
+------------------------------------------------------------------------------------------------
 
 Viikinkien suurimmat Jumalat olivat nimeltään Odin, Thor ja Freija. Odin muistetaan 
 erityisesti sodan jumalana. Vanha uskomus kuuluu, että Odin ratsasti kahdeksanjalkaisella
@@ -46,50 +53,66 @@ Freija muistetaan rauhan, hedelmällisyyden, sateen ja auringonpaisteen jumalana
 
 "mikä oli thorin vasaran nimi?"
 
+
+-----------------------------------------------------------------------------------------------
+
 Viikingit olivat kuuluisia myös tutkimusmatkoistaan. He löysivät muun muassa Islannin 
 ja Grönlannin, sekä muodostivat näihin maihin asumuksia ja yhteiskuntia jotka 
 kestivät vuosisatojen ajan. Viikinkien vaihtokauppareitit ulottuivat jopa 
 Aasiaan saakka, ja he ovat käyneet kauppaa myös Venäjän sekä eräiden 
 lähi-idän maiden kanssa. 
 
-
-
-
+"Mikä oli "
 
 
 */
-document.getElementById('answerform').addEventListener('submit', answer);
+
 
 
 const questions = [
 
     {
         
-      trivia: "Viikingit muistetaan keskuudessamme pohjoismaisina hurjina sotureina, ja useimmat aiheesta kertovat pelit, elokuvat ja tarinat keskittyvätkin suurimmaksi osaksi tähän heidän hurjaan puoleensa. Viikingit olivat kuitenkin ennen kaikkea menestyneitä kauppiaita ja liikkeenharjoittajia. He saivat elantonsa pitämällä maatiloja, kalastamalla, metsästämällä, kauppaamalla tekemiänsä tuotteita sekä valloittamalla uusia alueita. "  ,
-      question: "Mille aikavälille Viikinkien aikakausi sijoittui?",
+      trivia: "Viikingit muistetaan keskuudessamme pohjoismaisina hurjina sotureina, ja useimmat aiheesta kertovat pelit, elokuvat ja tarinat keskittyvätkin suurimmaksi osaksi tähän heidän hurjaan puoleensa. Viikingit olivat kuitenkin ennen kaikkea menestyneitä kauppiaita ja liikkeenharjoittajia. He saivat elantonsa pitämällä maatiloja, kalastamalla, metsästämällä, kauppaamalla tekemiänsä tuotteita sekä valloittamalla uusia alueita. KYSYMYS: Mille aikavälille Viikinkien aikakausi sijoittui? "  ,
       choices: ["650-859", "793-1066", "823-1145"], 
-      image: "./images/vikings0.jpg",
-      answer: 1
+      image: "",
+      answer: 1,
+      feedback: "Vastauksesi oli oikein! ölkjölkjölkjölkjölkj"
     } ,
 
 
     {
         
-        trivia: "Viikingit olivat pohjimmiltaan kuitenkin maalaisyhteisöjä, joissa perhe ja oman maapalstan omistaminen näyttelivät tärkeintä roolia. Maan omistaminen oli kaikkein tärkeintä, koska se määritteli ihmisen aseman yhteisössä. Iso perhe ja maatila olikin kaikista tavallisin näky viikinkiyhteisöissä. Naiset olivat kodin ylipäälliköitä, ja miehet hoitivat kaupankäynnin, pellonhoidon ja yhteiskunnalliset toiminnot."  ,
-        question: "Mikä oli tunnetuin Viikinkikaupunki?",
+        trivia: "Viikingit olivat pohjimmiltaan kuitenkin maalaisyhteisöjä, joissa perhe ja oman maapalstan omistaminen näyttelivät tärkeintä roolia. Maan omistaminen oli kaikkein tärkeintä, koska se määritteli ihmisen aseman yhteisössä. Iso perhe ja maatila olikin kaikista tavallisin näky viikinkiyhteisöissä. Naiset olivat kodin ylipäälliköitä, ja miehet hoitivat kaupankäynnin, pellonhoidon ja yhteiskunnalliset toiminnot. KYSYMYS: Mikä oli tunnetuin Viikinkikaupunki?"  ,
         choices: ["Hedeby", "Oslo", "Uppsala"],
-        image: "./images/vikings0.jpg",
-        answer: 0
+        image: "",
+        answer: 0,
+        feedback: "asdfasdfasdfasdf"
       }  ,
 
       {
-        trivia: "Viikinkien pitämiin eläimiin kuuluivat esimerkiksi vuohet, lampaat, siat, lampaat ja ankat. Niitä vaalittiin suuresti, sillä ne tuottivat Viikingeille ruokaa ja erilaisia, hyötytarvikkeita. Heillä oli myös lemmikkieläimiä (koiria ja kissoja) kuten meilläkin. Viikinkien jumaltarustoon kuitenkin kuului eräs tärkeä susieläin.", 
-        question: "Mikä on tämän kuuluisan suden nimi?",
+        trivia: "Viikinkien pitämiin eläimiin kuuluivat esimerkiksi vuohet, lampaat, siat, lampaat ja ankat. Niitä vaalittiin suuresti, sillä ne tuottivat Viikingeille ruokaa ja erilaisia, hyötytarvikkeita. Heillä oli myös lemmikkieläimiä (koiria ja kissoja) kuten meilläkin. Viikinkien jumaltarustoon kuitenkin kuului eräs tärkeä susieläin. KYSYMYS: Mikä on tämän kuuluisan suden nimi?", 
         choices: ["Gullinbursti","Jörmundgandr","Fenrir"],
-        image: "./images/vikings0.jpg",
-        answer: 2
-      } 
+        image: "",
+        answer: 2,
+        feedback: "qwerqwerqwerqwer"
+      } ,
 
+      {
+        trivia: "Viikinkien yhteiskunnassa oli kolme erilaista luokkaa kansalaisille. Ihmiset jaoteltiin aatelisiin (Jarl), vapaisiin ihmisiin (Karl) sekä orjiin ja maaorjiin. Jaarlit hallitsivat erikokoisia maa-alueita, ja vasta myöhemmässä vaiheessa viikinkien aikakaudella luokkiin tulivat mukaan kuninkaat ja kuningattaret. Viikinkimytologiassa ja historiassa ylipäätänsä on välillä vaikeata erottaa; kuka on ollut oikea ja merkittävä henkilö, ja kenen maine on kasvanut tarujen ja saagojen myötävaikutuksesta todellista suuremmaksi. Historiaa tutkineet akateemikot ovat kuitenkin yhtä mieltä siitä, että tietyt henkilöt ovat olleet suuria johtajia, sekä erittäin arvostettuja henkilöitä viikinkien keskuudessa. KYSYMYS: Kuka oli kaikista kuuluisin viikinki? ", 
+        choices: ["Björn Rautakylki","Erik Punainen","Ragnar Lothbrok"],
+        image: "",
+        answer: 2,
+        feedback: "Ragnar Lothbrokin olemassaolosta ei ole täyttä historiallista varmuutta, mutta hänestä on puhuttu paljon, sekä hänet on yhdistetty viikinkien tarustoissa moniin eri tapahtumiin ja paikkoihin. Hänet tunnettiin tarustojen ja legendojen perusteella Tanskan ja Ruotsin kuninkaana. Ragnar Lothbrok esiintyi myös Viikingit-nimisessä sarjassa, ja sarjan ensimmäiset osat keskittyivät hänen ympärille."
+      } ,
+
+      {
+        trivia: "Viikinkien suurimmat Jumalat olivat nimeltään Odin, Thor ja Freija. Odin muistetaan erityisesti sodan jumalana. Vanha uskomus kuuluu, että Odin ratsasti kahdeksanjalkaisella ratsulla ja Odinilla oli vain yksi silmä, koska hän vaihtoi toisen silmänsä yhteen kulaukseen juomaa viisauden lähteestä. Thor muistetaan oikeuden, voiman ja taistelukenttien jumalana. Thor puolusti ihmisiä jättiläisiltä. Thorilla oli myös kuuluisa vasara, ja hänen mukaansa on myös nimetty viikonpäivä torstai. Freija muistetaan rauhan, hedelmällisyyden, sateen ja auringonpaisteen jumalana. mikä oli thorin vasaran nimi?",
+        choices: ["Skofnung","Mjölnir","Hrolf"],
+        image: "",
+        answer: 1,
+        feedback: "Mjölnir tunnetaan Thorin vasarana. Sitä on käytetty erilaisissa peleissä, sarjakuvissa ja elokuvissa paljon. "
+      } 
   ];
   
 let currentQuestion = 0;
@@ -102,8 +125,8 @@ let points = 0;
     const triviaText = document.getElementById("triviabox");
     triviaText.textContent = questions[currentQuestion].trivia;
 
-    const questionText = document.getElementById("feedbackbox");
-    questionText.textContent = questions[currentQuestion].question;
+    const hideFeedback = document.getElementById("feedbackbox");
+    hideFeedback.textContent = "";
 
     const showImage = document.getElementById("imagebox");
     showImage.src = "./images/vikings" + currentQuestion + ".jpg";
@@ -116,8 +139,11 @@ let points = 0;
     });
 
 }
-
 showQuestion();
+
+/*-----------------------------------------------------------------------------------*/
+
+document.getElementById('answerform').addEventListener('submit', answer);
 
 function answer(e){
     e.preventDefault();
@@ -127,11 +153,20 @@ function answer(e){
 
     if( formdata.get('selection') ==  questions[currentQuestion].answer ){
         points++;
-
+        const correctAnswer = document.getElementById("feedbackbox");
+        correctAnswer.textContent = questions[currentQuestion].feedback;
         console.log("Points: ", points);
 }
-  nextQuestion();
+
+    else {
+      const wrongAnswer = document.getElementById("feedbackbox");
+      wrongAnswer.textContent = "Vastauksesi oli väärin";
+    }
+setTimeout(nextQuestion, 10000);
 }
+
+
+/*-----------------------------------------------------------------------------------*/
 
 function nextQuestion ()  {
     if (currentQuestion <= questions.length)
