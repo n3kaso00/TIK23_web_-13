bd2784f3a5545c00937f36d002e86dc1e4323f29
oliluoma <n3luol00@students.oamk.fi>
2024-04-18 13:32:56 +0300

    Vaidoin pelin ideaa ja aloitin html,css ja js

--- js/rome.js
+++ js/rome.js
@@ -1,56 +1,10 @@
-const wordList = [
-    {
-        word: "rooma",
-        hint: "kaupunki"
-    },
-    {
-        word: "colosseum",
-        hint: "areena"
-    }
-]
-
-const inputs = document.querySelector(".inputs"),
-resetBtn = document.querySelector(".reset-btn"),
-hint = document.querySelector(".hint span"),
-wrongLetter = document.querySelector(".wrong-letter span"),
-typingInput = document.querySelector(".typing-input");
-
-let word, incorrects = [];
-
-function randomWord() {
-    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
-    word = ranObj.word;
-    console.log(ranObj);
-
-    hint.innerText = ranObj.hint;
-
-    let html = "";
-    for (let i = 0; i < word.length; i++) {
-        html = `<input type="text" disabled>`;
-    }
-    inputs.innerHTML = html;
+let parent = document.getElementById('drag');
+let frag = document.createDocumentFragment();
+while (parent.children.lenght){
+    frag.appendChild(parent.children[Math.floor(Math.random() * parent.children.lenght)])
 }
+parent.appendChild(frag);
 
-randomWord();
-
-function initGame(e) {
-    let key = e.target.value;
-    if(key.match(/^[A-Za-z]+$/)) {
-        console.log(key);
-        if(word.includes(key)) {
-            for (let i = 0; i < array.length; i++) {
-                if(word[i] === key) {
-                    inputs.querySelectorAll("input")[i].value = key;
-                }
-            }
-        } else {
-            incorrects.push(key);
-        }
-    }
-    wrongLetter.innerText = incorrects;
-    typingInput.value = "";
-}
-
-resetBtn.addEventListener("click", randomWord);
-typingInput.addEventListener("input", initGame)
-document.addEventListener("keydown", () => typingInput.focus());
+document.querySelector('.reset').addEventListener('click', function() {
+    window.location.reload ()
+});
