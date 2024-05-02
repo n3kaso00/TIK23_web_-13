b021ca8697cd3978c427f97d1cdb3d900d5c2713
oliluoma <n3luol00@students.oamk.fi>
2024-05-02 15:36:49 +0300

    pisteiden muokkausta

--- js/points.js
+++ js/points.js
@@ -3,6 +3,7 @@ let vikingPoints = 0;
 let romePoints = 0;
 let industrialPoints = 0;
 
+
 displayVikingPoints()
 displayRomeMaxPoints()
 displayIndustrialPoints()
@@ -25,6 +26,8 @@ function displayIndustrialPoints() {
     const industrialScoreTable = document.getElementById('industrial_score');
     industrialScoreTable.innerText = industrialPoints;
 }
+
+
 
 /*pistehaku päättyy*/
 
