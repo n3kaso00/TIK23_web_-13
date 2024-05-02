01f7a6d3105d441726ba5436844d515b95694441
n3kaso00 <n3kaso00@students.oamk.fi>
2024-05-02 13:21:47 +0300

    industrial pisteiden välimuisti

--- js/points.js
+++ js/points.js
@@ -1,9 +1,11 @@
 
 let vikingPoints = 0;
 let romePoints = 0;
+let industrialPoints = 0;
 
 displayVikingPoints()
 displayRomeMaxPoints()
+displayIndustrialPoints()
 
 /*pistehaku*/
 
@@ -18,5 +20,12 @@ function displayRomeMaxPoints() {
     const romeScoreTable = document.getElementById('rome_score');
     romeScoreTable.innerText = romePoints;
 }
+
+function displayIndustrialPoints() {
+    industrialPoints = parseInt(localStorage.getItem('industrial_points')) || 0;
+    const industrialScoreTable = document.getElementById('industrial_score');
+    industrialScoreTable.innerText = industrialPoints;
+}
+
 /*pistehaku päättyy*/
 
